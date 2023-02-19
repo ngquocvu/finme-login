c493e79cb206f9aaf6ddadec48af4458b7f4c058
Vu Nguyen <vu_nguyen@epam.com>
2023-02-19 18:20:24 +0700

    backend for finme

--- client/src/helpers/token.js
+++
@@ -1,6 +0,0 @@
-import jwtDecode from "jwt-decode";
-export function getUsernameFromToken() {
-  const token = localStorage.getItem("token");
-  let decode = token && jwtDecode(token);
-  return decode?.username;
-}
